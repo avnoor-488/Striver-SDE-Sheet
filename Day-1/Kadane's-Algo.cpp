5cfe672eb1004f16c21742c11e62f2a367bf9b5b
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-04-16 01:07:58 +0530

    Create Kadane's-Algo.cpp

---
+++ Day-1/Kadane's-Algo.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+
+typedef pair<int, int> pii;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+
+
+void readVec(vi &vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+}
+
+
+void printVec(vi vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+
+void solve(vi vec)
+{
+
+    int totalSum = INT_MIN, maxi = 0;
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        maxi = max(maxi + vec[i], vec[i]);
+        totalSum = max(totalSum, maxi);
+    }
+
+    cout << totalSum;
+}
+
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n ;
+        cout << n ;
+        vi vec(n);
+        for (int i = 0; i < n; i++)
+        {
+
+            cin >> vec[i];
+
+        }
+
+
+
+        cout << endl;
+
+        solve(vec);
+        // cout << vec.size();
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
