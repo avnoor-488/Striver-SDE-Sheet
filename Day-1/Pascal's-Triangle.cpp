e26cc8c75ee7e6d506b289a2800c24669bec65c6
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-04-15 23:14:49 +0530

    Create Pascal's-Triangle.cpp

---
+++ Day-1/Pascal's-Triangle.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+
+typedef pair<int, int> pii;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+
+
+void readVec(vi &vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+}
+
+
+void printVec(vi vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+
+vector< vector<int> > solve(int n)
+{
+    vector< vector<int> > vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        vec[i].resize(i + 1);
+        vec[i][0] = vec[i][i] = 1;
+
+        for (int j = 1; j < i; j++)
+            vec[i][j] = vec[i - 1][j - 1] + vec[i - 1][j];
+    } 
+    return vec;
+}
+
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n ;
+        cout << n ;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     for (int j = 0; j < m; j++)
+        //     {
+        //         cin >> vec[i][j];
+        //     }
+
+        // }
+
+        cout << endl;
+        vector< vector<int> > vec = solve(n);
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
