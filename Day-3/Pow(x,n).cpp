87934c1c8426ffae19afd95fdabf34c89dc634b0
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-04-27 13:28:30 +0530

    Create Pow(x,n).cpp

---
+++ Day-3/Pow(x,n).cpp
@@ -0,0 +1,106 @@
+// Probelem Link :- https://leetcode.com/problems/powx-n/submissions/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define num 4e6 + 5
+
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+ll dp[100010][110][2];
+ll arr[100010];
+
+double power(double x, int y)
+{
+
+    double temp = 0;
+
+    if (y == 0)
+        return 1;
+
+    temp = power(x, (y / 2));
+
+    if (y % 2 == 0)
+        return temp * temp;
+    else
+    {
+        if (y > 0)
+            return x * temp * temp;
+        else
+            return (temp * temp) / x;
+    }
+}
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+
+    {
+        double x; cin >> x;
+        int n; cin >> n;
+        double ans = power(x, n);
+        cout << fixed << setprecision(6);
+        cout  << ans  << " ";
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
