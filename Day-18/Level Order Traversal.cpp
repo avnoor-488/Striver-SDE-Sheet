162efb3b8ca57bc6ad7767b353a8abe5683db80f
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-07-02 23:56:33 +0530

    Create Level Order Traversal.cpp

---
+++ Day-18/Level Order Traversal.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+        vector<vector<int>>output;
+        
+        if(root==NULL)
+            return output;
+        
+        queue<TreeNode *>q;
+        q.push(root);
+        q.push(NULL);
+        
+        vector<int>ans;
+        while(!q.empty())
+        {
+            TreeNode *front = q.front();
+            q.pop();
+            if(front ==NULL)
+               {
+
+                output.push_back(ans);
+                ans.resize(0);
+                if(!q.empty())
+                    q.push(NULL);
+                // cout<<output.size();
+                }
+            else 
+            {
+                ans.push_back(front->val);
+                if(front->left)
+                    q.push(front->left);
+                if(front->right)
+                    q.push(front->right);
+             }           
+            
+        }      
+        
+        return output;
+    }
+};
